56033eb5045a0cb99e6135f086218c2484130288
tscz <tscz@github.com>
2024-02-24 22:24:05 +0100

    use peaks.js-react in example app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useCustomHook } from "peaks.js-react";
 
 function App() {
+  const { data, loading, error } = useCustomHook();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Demo for peaks.js-react</h1>
+      {!!loading && <p>Loading</p>}
+      {!!error && <p>Error: {JSON.stringify(error)}</p>}
+      {!error && !loading && !!data && <p>{data}</p>}
     </div>
   );
 }
