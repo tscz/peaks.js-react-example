a2d32aa33602f0898ea40d99f1a218685f4e55f5
tscz <tscz@github.com>
2024-02-25 01:44:10 +0100

    use usePeaks hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { useCustomHook } from "peaks.js-react";
+import { usePeaks } from "peaks.js-react";
 
 function App() {
-  const { data, loading, error } = useCustomHook();
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const waveformRef = useRef<HTMLDivElement>(null);
+
+  const { peaks, loading, error } = usePeaks({
+    audioRef,
+    waveformRef,
+    options: {},
+  });
 
   return (
     <div className="App">
       <h1>Demo for peaks.js-react</h1>
+      <div
+        id="waveform"
+        ref={waveformRef}
+        style={{ width: "400px", height: "200px" }}
+      ></div>
+      <audio id="audio" ref={audioRef}></audio>
       {!!loading && <p>Loading</p>}
-      {!!error && <p>Error: {JSON.stringify(error)}</p>}
-      {!error && !loading && !!data && <p>{data}</p>}
+      {!!error && <p>Error: {error.message}</p>}
+      {!error && !loading && !!peaks && (
+        <p>{`current zoom:${peaks.zoom.getZoom()}`}</p>
+      )}
     </div>
   );
 }
